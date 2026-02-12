51f616c55a1258f907348b8f4ee6667c17bfdc58
Milan Černý <milancerny2@seznam.cz>
2026-02-12 19:12:46 +0100

    Fix test prompt

--- tests/prompt_generator_test.py
+++ tests/prompt_generator_test.py
@@ -1,7 +1,7 @@
 from src.prompt_generator.prompt_generator import PromptGenerator 
 
 
-TEST_PROMPT="A realistic selfie photo of a 58-year-old male person from Argentina, with miserable mood and pained expression expression, shade under trees lighting, with in an office background, and additional camera details: realistic photography style."
+TEST_PROMPT="A realistic selfie photo of a 58-year-old male person from Argentina, with miserable mood and pained expression, shade under trees lighting, with in an office background, and additional camera details: realistic photography style."
 
 def test_prompt_generator():
     generator: PromptGenerator = PromptGenerator(seed=42)
